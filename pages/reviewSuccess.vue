7b7c28983afd10fefdfb1ae9fcf81b8f024d0a85
xn <280145668@qq.com>
2022-09-25 21:34:35 +0800

    deploy

--- pages/reviewSuccess.vue
+++ pages/reviewSuccess.vue
@@ -1,3 +1,12 @@
 <template>
-  <x-alert type="success" center="ok">评价成功, 感谢您的参与!</x-alert>
+  <NuxtLayout name="review"><x-alert type="success" center="ok">评价成功, 感谢您的参与!</x-alert></NuxtLayout>
 </template>
+
+<script setup>
+definePageMeta({
+  layout: false,
+});
+useHead({
+  title: `江安社区党建`,
+});
+</script>
