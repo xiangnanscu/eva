167cdaa002e51a48dce74b16dad88fab33fd8447
xn <280145668@qq.com>
2022-10-06 00:08:38 +0800

    deploy1

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,8 +2,12 @@
   <div class="container">
     <nav class="navbar navbar-expand-lg bg-light mb-2">
       <div class="container-fluid">
-        <a class="navbar-brand" href="/"><img src="/jahy.svg" alt="打造社区党建第一品牌" width="40"
-            height="40" style="margin:0 5px"/>江安社区党建</a>
+        <a class="navbar-brand" href="/"><img src="/jahy.svg" alt="打造社区党建第一品牌" width="40" height="40"
+            style="margin:0 5px" />江安社区党建</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
