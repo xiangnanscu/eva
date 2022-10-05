54cbb4232f1c18e9ab6e489a4fb0d9c05e1056d2
xn <280145668@qq.com>
2022-10-05 23:56:10 +0800

    deploy1

--- app.vue
+++ app.vue
@@ -3,6 +3,7 @@
 
     <Head>
       <Link rel="icon" type="image/png" sizes="192x192" href="/jahy.svg" />
+      <Link rel="stylesheet" type="text/css" href="/bootstrap.min.css" />
     </Head>
     <NuxtPage></NuxtPage>
   </NuxtLayout>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,8 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   ssr: true,
-  // modules: ['bootstrap-vue-3/nuxt'],
-  css: ["bootstrap/dist/css/bootstrap.css"],
+  // modules: ['bootstrap/dist/css/bootstrap.css'],
+  // css: ["bootstrap/dist/css/bootstrap.css"],
   // plugins: [{ src: "~/plugins/rating.js", mode: "client" }],
   nitro: {
     preset: "node-server",
